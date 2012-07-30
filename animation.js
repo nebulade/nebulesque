d7716886ba4b156ad28b0abca64df7a0d15dc288
Johannes Zellner <webmaster@nebulon.de>
2012-07-30 13:04:51 -0700

    add animation.js

---
+++ animation.js
@@ -0,0 +1,36 @@
+
+function NumberAnimation(element, property, from, to, duration) {
+	this.element = element;
+	this.property = property;
+	this.from = parseFloat(from);
+	this.to = parseFloat(to);
+	this.reverse = this.to > this.from ? false : true;
+	this.value = this.from;
+	this.duration = duration ? parseInt(duration) : 250;
+	this.distance = this.reverse ? this.from - this.to : this.to - this.from;
+	this.step = 16;
+}
+
+NumberAnimation.prototype = {
+	advance : function advance() {
+		if (this.reverse) {
+			if (this.value <= this.to)
+				return;
+			this.value -= this.distance/(this.duration/this.step) * 1.0;
+		} else { 
+			if (this.value >= this.to)
+				return;
+			this.value += this.distance/(this.duration/this.step) * 1.0;
+		}
+		
+		this.element[this.property] = this.value;
+		
+		var _this = this;
+		window.setTimeout(function () { _this.advance(); }, this.step);
+	},
+	
+	start : function start() {
+		this.element[this.property] = this.value;
+		this.advance();
+	}
+}
